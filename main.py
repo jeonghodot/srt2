add48f3f0d12bcf29e48f1f40ef582dcc37a226b
jeonghodot <jeonghodot@gmail.com>
2023-09-28 22:39:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ if __name__ == "__main__":
     parser.add_argument('--dep', default="수서")
     parser.add_argument('--arr', default="동대구")
     parser.add_argument('--date', default="20230929")
-    parser.add_argument('--start', default="090000")
-    parser.add_argument('--end', default="140000")
-    parser.add_argument('--iter', default="1000")
-    parser.add_argument('--seat', default="일반실")
+    parser.add_argument('--start', default="101000")
+    parser.add_argument('--end', default="120000")
+    parser.add_argument('--iter', default="999999")
+    # parser.add_argument('--seat', default="일반실")
     args = parser.parse_args()
     print(args)
 
@@ -33,31 +33,51 @@ if __name__ == "__main__":
     time = args.start
     time_limit = args.end
     iter = args.iter
-    seatType = args.seat
+    # seatType = args.seat
 
-    for k in range(1, int(iter)+1):
-        
+    for k in range(1, int(iter)+1):   
+        print("\r"+str(k), end="")
         try: 
-            trains = srt.search_train(dep, arr, date, time, time_limit)
-            # print(trains)
+            # print(srt.search_train(dep, arr, "20231005", time))
+
+            reservation = srt.reserve(srt.search_train(dep, arr, date, time, time_limit)[0])
+            bot.sendMessage(chat_id=chatId, text="[SRT]" + str(reservation))
+            print(reservation)
+            
+            reservation = srt.reserve(srt.search_train(dep, arr, date, time, time_limit)[0])
+            bot.sendMessage(chat_id=chatId, text="[SRT]" + str(reservation))
+            print(reservation)
         except:
             timer.sleep(random.uniform(0.0, 2.0))
-            trains = srt.search_train(dep, arr, date, time, time_limit)
-            # continue 
-        print("\r"+str(k), end="")
-        
-        for i in range(0, len(trains)):
-            print(trains[i])
-        j = 0
-        for j in range(0, len(trains)):
-            if seatType in str(trains[j]):
-                try:
-                    reservation = srt.reserve(trains[j])
-                    print(reservation)
-                    bot.sendMessage(chat_id=chatId, text="[SRT]" + str(reservation))
-                    continue
-                except:
-                    continue
-        timer.sleep(random.uniform(0.0, 2.0))
+            continue
     bot.sendMessage(chat_id=chatId, text="[SRT] Reservation finished")
+    
+
+    # for k in range(1, int(iter)+1):      
+    #     try: 
+    #         trains = srt.search_train(dep, arr, date, time, time_limit)
+    #         # print(trains)
+    #     except:
+    #         print('except1')
+    #         timer.sleep(random.uniform(0.0, 2.0))
+    #         trains = srt.search_train(dep, arr, date, time, time_limit)
+    #         print(trains)
+    #         # continue 
+    #     print("\r"+str(k), end="")
+        
+    #     for i in range(0, len(trains)):
+    #         print(trains[i])
+    #     j = 0
+    #     for j in range(0, len(trains)):
+    #         if seatType in str(trains[j]):
+    #             try:
+    #                 reservation = srt.reserve(trains[j])
+    #                 print(reservation)
+    #                 bot.sendMessage(chat_id=chatId, text="[SRT]" + str(reservation))
+    #                 continue
+    #             except:
+    #                 print('except')
+    #                 continue
+    #     timer.sleep(random.uniform(0.0, 2.0))
+    # bot.sendMessage(chat_id=chatId, text="[SRT] Reservation finished")
     
