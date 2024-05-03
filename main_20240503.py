ee4ec08963f2cf141d571c2543c7daed32df4a3c
jeonghodot <jeonghodot@gmail.com>
2024-05-03 13:05:01 +0900

    Create main_20240503.py

---
+++ main_20240503.py
@@ -0,0 +1,79 @@
+#pip install python-telegram-bot==13.14
+
+from SRT import SRT ,SeatType, Adult, Child
+import telegram
+import time as timer
+import argparse
+import random
+import os
+import sys
+import subprocess
+
+def restart():
+    python_executable = sys.executable
+    script_path = sys.argv[0]
+    
+    subprocess.run([python_executable, script_path])
+    
+if __name__ == "__main__":
+    api_key = '5957545379:AAFAHTqrMAqMAWUsD9EkT3XvLcsu0snQgRU'
+    chatId = '5517769456'
+    bot = telegram.Bot(token=api_key)
+    for item in bot.getUpdates():
+        print(item)
+    
+    parser = argparse.ArgumentParser(description='SRT Reservation')
+    parser.add_argument('--id',     default="010-4442-5552")
+    parser.add_argument('--pw',     default="Dighwjdgh3#")
+    parser.add_argument('--dep',    default="수서")
+    parser.add_argument('--arr',    default="동대구")
+    parser.add_argument('--date',   default="20240503")
+    parser.add_argument('--start',  default="192000")
+    parser.add_argument('--end',    default="203000")
+    parser.add_argument('--iter',   default="9999999")
+    parser.add_argument('--seat', default=SeatType.GENERAL_FIRST)
+    args = parser.parse_args()
+    print(args)
+
+    srt = SRT(args.id, args.pw)
+    dep = args.dep
+    arr = args.arr
+    date = args.date
+    time = args.start
+    time_limit = args.end
+    iter = args.iter
+    seatType = args.seat
+    
+    psg=[Adult(), Adult()]
+    # psg=[Adult()]=
+
+    for k in range(1, int(iter)+1):
+        print("\r"+str(k), end="")
+        try:
+            # print(srt.search_train(dep, arr, date, time, time_limit))
+            for i in range(3) :
+                reservation = srt.reserve(srt.search_train(dep, arr, date, time, time_limit)[0], passengers=psg)
+                # reservation = srt.reserve(srt.search_train(dep, arr, date, time, time_limit)[0], special_seat=seatType, passengers=psg)
+                bot.sendMessage(chat_id=chatId, text=str(reservation))
+                print(reservation) 
+                
+        except IndexError as e:
+            timer.sleep(random.uniform(0.0, 2.0))
+            continue
+        except Exception as f:
+            print(f)
+            #print("script restarted")
+            restart()
+            continue
+        
+    bot.sendMessage(chat_id=chatId, text="[SRT] Reservation finished")
+    
+    
+    # seatType=SeatType.SPECIAL_ONLY
+    # 1 SeatType.GENERAL_FIRST : 일반실 우선
+    # 2 SeatType.GENERAL_ONLY : 일반실만
+    # 3 SeatType.SPECIAL_FIRST : 특실 우선
+    # 4 SeatType.SPECIAL_ONLY : 특실만
+    # special_seat: SeatType = SeatType.GENERAL_FIRST
+
+    
